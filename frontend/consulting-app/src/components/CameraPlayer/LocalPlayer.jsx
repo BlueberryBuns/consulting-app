177887a634ad417f0475f8af91392a8f377b09ad
HulewiczKamil <hulewicz.k@gmail.com>
2021-11-09 16:19:27 +0100

    Add connecting and disconnecting scrnario handling by client

--- frontend/consulting-app/src/components/CameraPlayer/LocalPlayer.jsx
+++ frontend/consulting-app/src/components/CameraPlayer/LocalPlayer.jsx
@@ -3,13 +3,82 @@ import { useStore } from "../../stores/custom-store/store";
 import initialCameraSetup from "../../media-utils/base-config";
 import { mediaMapping } from "../../media-utils/mappings";
 import CameraPlayer from "./CameraPlayer";
+import { useSelector } from "react-redux";
 
 const CAMERA_CONFIG = { ...initialCameraSetup };
 let isCameraSet = false;
 
 const LocalPlayer = (props) => {
+  const authState = useSelector((state) => state.account);
   const [globalStore, dispatch] = useStore();
   const videoRef = useRef();
+
+  let location = window.location;
+  let webSocketType = "ws://";
+
+  if (location.protocol == "https:") {
+    webSocketType = "wss://";
+  }
+  const serverAddr = "localhost:8000";
+  // console.log(serverAddr);
+  let wSocketAddr =
+    webSocketType +
+    serverAddr +
+    "/ws/chat/room/123/?token=" +
+    authState.accessToken; // + location.pathname;
+
+  // console.log(wSocketAddr);
+
+  const ws = new WebSocket(wSocketAddr);
+
+  ws.onopen = (event) => {
+    // Joins or creates room
+    console.log("Connection Opened");
+    const message = JSON.stringify({
+      type: "share.session.description.protocol",
+      message: "Example SDP",
+    });
+
+    console.log(event);
+    ws.send(message);
+  };
+
+  ws.onclose = (event) => {
+    console.log("Connection closed");
+    console.log(event);
+  };
+
+  ws.onerror = (event) => {
+    console.log("An error occured during processing websocket connection");
+    console.log(event);
+  };
+
+  ws.onmessage = (event) => {
+    //add switch with event handling
+    switch (JSON.parse(event.data).type) {
+      case "received.sdp":
+        ws.send(
+          JSON.stringify({
+            type: "hello",
+            message: "world",
+          })
+        );
+        break;
+
+      case "hello":
+        console.log(event.data);
+        break;
+
+      case "bye":
+        console.log(event.data);
+        break;
+
+      default:
+        break;
+    }
+    // console.log("Message received");
+    // console.log(event);
+  };
 
   const assignCamera = () => {
     /**TODO (hulewicz) Add error handling in case of both audio and video
